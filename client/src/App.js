673bfb553bdf46d05fe3e641af6db06ac23ee7fd
s-c-sims <sydney.csims@gmail.com>
2021-09-14 12:00:44 -0400

    .

--- client/src/App.js
+++ client/src/App.js
@@ -14,8 +14,6 @@ const App = () =>
 
   };
 
-
-
   const getBackend = async() =>
   {
     await fetch('/get')
@@ -38,7 +36,7 @@ const App = () =>
   return (
     <div className="App">
 
-      <h1>Hello World</h1>
+      <h1>React Express</h1>
 
       <button onClick={getBackend}>GET to backend</button>
 
